f8b11aa34c55378531a0427c54d730c87c6d7b8c
Tyter <knighthornor@163.com>
2022-09-03 13:52:56 +0800

    Just For Test3

--- tmp.java
+++ tmp.java
@@ -1,26 +1,42 @@
 import java.util.Scanner;
+import java.util.*;
 
 public class tmp
 {
     public static void main(String[] args)
     {
-        System.out.println("请输入若干个数字，每输入一个数字并回车确认.");
-        System.out.println("最后输入数字 0 来结束输入");
-        Scanner reader = new Scanner(System.in);
-        double sum = 0;
-        double x = reader.nextDouble();
-        while(x != 0 )
-        {
-            sum = sum + x;
-            x = reader.nextDouble();
-        }
-        System.out.println("Sum=" + sum);
-
-
-        float boy[] = new float[4];
+        PeopleName Jace = new PeopleName("Hello World");
+        Jace.speak();
 
     }
 }
+
+class PeopleName
+{
+    float height;
+    float weight;
+    String Sk;
+
+    PeopleName(String Se)
+    {
+        Sk = Se;
+        System.out.println("Entity Initial....\n");
+    }
+
+    PeopleName()
+    {
+
+    }
+
+    void speak()
+    {
+        System.out.println(Sk);
+    }
+}
+
+
+
+
 
 class Student
 {
