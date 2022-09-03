11d5712bd5c51f941d2f7f90db09e7821c26752d
Tyter <knighthornor@163.com>
2022-09-03 17:00:37 +0800

    testJust For Test4

--- tmp.java
+++ tmp.java
@@ -5,10 +5,51 @@ public class tmp
 {
     public static void main(String[] args)
     {
-        PeopleName Jace = new PeopleName("Hello World");
-        Jace.speak();
+        Point p1;
+        Point p2;
+
+        p1 = new Point();
+        p2 = new Point();
+
+        System.out.println("p1的引用：" + p1);
+        System.out.println("p2的引用： " + p2);
+
+        p1.setXY(1111,2222);
+        p2.setXY(-100, -200);
+
+        System.out.println("p1的x坐标：" + p1.x + ",y坐标：" + p1.y);
+        System.out.println("p2的x坐标：" + p2.x + ",y坐标：" + p2.y);
+
+        p1 = p2;
+
+        System.out.println("p2的值赋给p1之后\n");
+
+        System.out.println("p1的引用：" + p1);
+        System.out.println("p2的引用： " + p2);
+
+        p1.setXY(1111,2222);
+        p2.setXY(-100, -200);
+
+        System.out.println("p1的x坐标：" + p1.x + ",y坐标：" + p1.y);
+        System.out.println("p2的x坐标：" + p2.x + ",y坐标：" + p2.y);
+
+
 
     }
+}
+
+class Point
+{
+    int x;
+    int y;
+    void setXY(int m, int n)
+    {
+        x = m;
+        y = n;
+    }
+
+    Point()
+    {}
 }
 
 class PeopleName
